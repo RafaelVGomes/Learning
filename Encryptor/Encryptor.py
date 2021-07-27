5907c94b9d3f274d38c0dd45bcf2a33bd5d834fe
Rafael Gomes <rafavgomes@yahoo.com>
2021-07-27 18:35:51 -0300

    IMPROVEMENT! Now it can decrypt as well.

--- Encryptor/Encryptor.py
+++ Encryptor/Encryptor.py
@@ -1,6 +1,7 @@
 from os import system
 system('cls')
-#This program should encrypt sentences using a key between 1 and 24. And can't change numbers and any other char.
+#This program should encrypt (and now also decrypt) sentences using a key between -24 and 24, except 0.
+#And can't change numbers and any other char. (Challenge asked for this.)
 #Mixing portuguese and english language on porpose.
 
 inp = input("Digite texto para codificar: ")
@@ -10,44 +11,77 @@ inp = list(inp)
 svStatus = False
 while svStatus == False:
     try:
-        sv = int(input("Número codificador (1 ~ 24): "))
+        sv = int(input('''
+    Número codificador (1 ~ 24)
+    ou decodificador (-24 ~ -1): '''))
 
-        if sv < 1 or sv > 24:
-            print("Número inválido.")
+        if sv > 24 or sv < -24:
+            print("    Número inválido.")
             svStatus = False
         
+        elif sv == 0:
+            print("    Número inválido.")
+            svStatus = False
+
         else:
             svStatus = True
 
     except ValueError:
-        print("Entrada inválida. Use apenas números.")
+        print("    Entrada inválida. Use apenas números.")
         svStatus = False
 
 #Encrypted sentence at the end.
 cipher = ''
 
-for char in inp:
-    if not char.isalpha():
-        cipher += char
-        print("C0:", char, ord(char))
-        continue
-    
-    code = ord(char) + sv
-    
-    if code > 90 and code < 97:
-        code3 = (code - 91) + 65
-        print("C2:", chr(code3), code3)
-        cipher += chr(code3)
+if sv > 0:
+#Encrypt
+    for char in inp:
+        if not char.isalpha():
+            cipher += char
+            #print("C0:", char, ord(char))
+            continue
+        
+        code = ord(char) + sv
+        
+        if code > 90 and code < 97:
+            code3 = (code - 91) + 65
+            #print("C2:", chr(code3), code3)
+            cipher += chr(code3)
+        elif code > 122:
+            code2 = (code - 123) + 97
+            #print("C2:", chr(code2), code2)
+            cipher += chr(code2)
+        else:
+            #print("C1:", chr(code), code)
+            cipher += chr(code)
+    print()
+    print("Criptografado:", cipher)
+    print()
 
-    elif code > 122:
-        code2 = (code - 123) + 97
-        print("C2:", chr(code2), code2)
-        cipher += chr(code2)
+#Decrypt
+else:
+    for char in inp:
+        if not char.isalpha():
+            cipher += char
+            #print("-C0:", char, ord(char))
+            continue
+        
+        code = ord(char) + sv
+        
+        if code < 65:
+            code3 = (code + 91) - 65
+            #print("-C2:", chr(code3), code3)
+            cipher += chr(code3)
 
-    else:
-        print("C1:", chr(code), code)
-        cipher += chr(code)
-   
-print()
-print("Criptografado:", cipher)
+        elif code < 97 and code > 90:
+            code2 = (code + 123) - 97
+            #print("-C2:", chr(code2), code2)
+            cipher += chr(code2)
+
+        else:
+            #print("-C1:", chr(code), code)
+            cipher += chr(code)
+    print()
+    print("Decriptografado:", cipher)
+            
 print()
