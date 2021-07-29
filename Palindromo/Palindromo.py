bc03fd1888a5aa6f0ffbefa3f7465576d37731ac
Rafael Gomes <rafavgomes@yahoo.com>
2021-07-29 11:58:32 -0300

    Short program to verify if a word is a palindrome

--- Palindromo/Palindromo.py
+++ Palindromo/Palindromo.py
@@ -7,15 +7,15 @@ UserInput = input("Enter a word: ")
 UserInput = UserInput.lower()
 reverse = []
 
-#Reversing user's input
+# Reversing user's input
 for i in UserInput:
     reverse.insert(0, i)
 
-#Formating reversed input
+# Formating reversed input
 reverse = ''.join(reverse)
 reverse = reverse.lower()
 
-#Checks if UserInput and reverse are the same word
+# Checks if UserInput and reverse are the same word
 print()
 if reverse == UserInput:
     print("It's a palindrome.")
