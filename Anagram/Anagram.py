1256bd321b39c712b36b291454b0e7f58fbcb1d3
Rafael Gomes <rafavgomes@yahoo.com>
2021-07-30 14:43:39 -0300

    Teste de commit

--- Anagram/Anagram.py
+++ Anagram/Anagram.py
@@ -2,12 +2,14 @@ from os import system
 system('cls')
 
 # User's input
-UserInput1 = "Listen".lower()
-UserInput2 = "Silent".lower()
+UserInput1 = list(input("Digite: ").lower())
+UserInput2 = list(input("Digite: ").lower())
 
 # Comparing words letters
 def AnagramCheck(word1, word2):
-    if word1 in word2:
+    word1.sort()
+    word2.sort()
+    if word1 == word2:
         print("\n", "Is a anagram.", "\n")
     else:
         print("\n", "Is not a anagram.", "\n")
