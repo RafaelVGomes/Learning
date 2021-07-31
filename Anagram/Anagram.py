9d8f7c2a8d743a43791e2b0fc800ade758921382
Rafael Gomes <rafavgomes@yahoo.com>
2021-07-31 13:09:33 -0300

    Finally DONE!

--- Anagram/Anagram.py
+++ Anagram/Anagram.py
@@ -1,29 +1,48 @@
+#/usr/bin/python3
 from os import system
 system('cls')
 
 # Comparing words
 def WordsCheck():
+    global UserInput1
+    global UserInput2
     st = False
     while st == False:   
-        # User's input
-        UserInput1 = input("Digite uma palavra: ").lower()
-        UserInput2 = input("Digite outra palavra: ").lower()
-        if UserInput1 == "0" or UserInput2 == 0:
+        # First user's input and "0" to EXIT
+        print("Digite 0 para sair.")
+        UserInput1 = input("Digite uma palavra: ")
+        if UserInput1 == "0":
             exit()
-        elif UserInput1 == UserInput2:
+        
+        # Second user's
+        UserInput2 = input("Digite outra palavra: ")
+        
+        # Treating UserInput1 and 2 as lower case
+        ui1 = UserInput1.lower()
+        ui2 = UserInput2.lower()
+        
+        # Comparing Words. If different calls AnagramCheck()
+        if ui1 == ui2:
             st == False
-            print("\n", "Use palavras diferentes. Digite 0 para sair.", "\n")
+            system('cls')
+            print("Use palavras diferentes.", "\n")
         else:
-            AnagramCheck(UserInput1, UserInput2)
+            AnagramCheck(ui1, ui2)
 
 # Comparing words letters
 def AnagramCheck(word1, word2):
-    if word1 != word2:
-        word1.sort()
-        word2.sort()
-    elif word1 == word2:
-        print("\n", "Is a anagram.", "\n")
+    # Treating lower cases entries as lists and sorting them
+    word1 = list(word1)
+    word2 = list(word2)
+    word1.sort()
+    word2.sort()
+    
+    # If all letters matchs or not, print an answer
+    if word1 == word2:
+        system('cls')
+        print("\n", UserInput1, "e", UserInput2, "são anagramas.", "\n")
     else:
-        print("\n", "Is not a anagram.", "\n")
+        system('cls')
+        print("\n", UserInput1, "e", UserInput2, "não são anagramas.", "\n")
 
 WordsCheck()
