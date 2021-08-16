904d493efe02e54c1929ed228eed9004747a9810
Rafael Gomes <rafavgomes@yahoo.com>
2021-08-16 11:39:37 -0300

    cleaning

--- Django/tutorial/trydjango/src/trydjango/urls.py
+++ Django/tutorial/trydjango/src/trydjango/urls.py
@@ -13,17 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from products.views import product_delete_view
 from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
-from products.views import product_detail_view
+from products.views import (
+    product_detail_view,
+    product_create_view,
+    render_initial_data,
+    dynamic_lookup_view,
+    product_delete_view
+    ) 
 
 urlpatterns = [
+    path('products/<int:id>/delete', product_delete_view, name='product-delete'),
+    path('products/<int:id>/', dynamic_lookup_view, name='product'),
+    
     path('', home_view, name='home'),
     path('home/', home_view),
     path('about/', about_view),
     path('contact/', contact_view),
-    path('product/', product_detail_view),
+    path('initial/', render_initial_data),
+    path('create/', product_create_view),
+    path('products/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
