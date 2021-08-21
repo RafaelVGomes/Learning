0863991938aa1668ec93d26ed6541cb1c818f689
Rafael Gomes <rafavgomes@yahoo.com>
2021-08-21 13:13:45 -0300

    Tabuleiro criado.

--- Coded in Python (OOP)/Jogo da Velha (OOP).py
+++ Coded in Python (OOP)/Jogo da Velha (OOP).py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+import numpy as np
 from os import system, name
 cls = lambda: system('cls' if name=='nt' else 'clear')
 
 class JogoDaVelha:
-    
     def __init__(self, lista):
         self.lista = lista
-        print("+---+---+---+")
-        print("|", lista[0][0], "|", lista[0][1], "|", lista[0][2], "|")
-        print("+---+---+---+")
-        print("|", lista[1][0], "|", lista[1][1], "|", lista[1][2], "|")
-        print("+---+---+---+")
-        print("|", lista[2][0], "|", lista[2][1], "|", lista[2][2], "|")
-        print("+---+---+---+")
+        
+    def Tabuleiro(self):
+        print('+---+---+---+')
+        print('|', self.lista[0][0] ,'|', self.lista[0][1] ,'|', self.lista[0][2] ,'|')
+        print('+---+---+---+')
+        print('|', self.lista[1][0] ,'|', self.lista[1][1] ,'|', self.lista[1][2] ,'|')
+        print('+---+---+---+')
+        print('|', self.lista[2][0] ,'|', self.lista[2][1] ,'|', self.lista[2][2] ,'|')
+        print('+---+---+---+')
+        pass
 
     def CasasLivres(self):
-        pass
+        if self.lista != "X" and self.lista != "O":
+            pass
     
     def Jogador(self):
         pass
@@ -30,8 +34,12 @@ class JogoDaVelha:
         pass
 
 casas = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
-n1 = 1
-n2 = 1
+casasvazias = []
+# n1 = 1
+# n2 = 1
 # casas[n1][n2] = "X"
 
-JogoDaVelha(casas)
+cls()
+obj = JogoDaVelha(casas)
+
+print(obj.Tabuleiro())
