984bd012baf018b84345b2d0b39e34703163a5e6
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2022-07-13 21:36:03 -0300

    admin area created. Starting index template

--- Python/W3/django/myworld/polls/views.py
+++ Python/W3/django/myworld/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse as res
 
-def index(req):
-  return res("Hello, world. You're at the polls index.")
+from .models import Question
 
+def index(req):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  output = ', '.join([q.question_text for q in latest_question_list])
+  return res(output)
+
+def detail(req, question_id):
+  return res(f"You're looking at question: {question_id}")
+
+def results(req, question_id):
+  return res(f"You're looking at the results of: {question_id}")
+
+def vote(req, question_id):
+  return res(f"You're voting on question: {question_id}")
