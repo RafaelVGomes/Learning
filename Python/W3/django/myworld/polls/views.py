bfa83edc8ef17195950894bc4b1938963f9ddb41
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2022-07-13 22:52:17 -0300

    end of 'views'...

--- Python/W3/django/myworld/polls/views.py
+++ Python/W3/django/myworld/polls/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse as res
 
 from .models import Question
 
 def index(req):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  output = ', '.join([q.question_text for q in latest_question_list])
-  return res(output)
+  context = {
+    'latest_question_list': latest_question_list
+  }
+  return render(req, 'polls/index.html',context)
 
 def detail(req, question_id):
-  return res(f"You're looking at question: {question_id}")
+  question = get_object_or_404(Question, pk=question_id)
+  return render(req, 'polls/detail.html', {'question': question})
 
 def results(req, question_id):
   return res(f"You're looking at the results of: {question_id}")
