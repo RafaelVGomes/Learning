83c8c3b10291bab386ccb268beebdfd8143a7033
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2022-07-17 17:47:49 -0300

    tests created and working

--- Python/W3/django/myworld/polls/views.py
+++ Python/W3/django/myworld/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse as res
 from django.http import HttpResponseRedirect as red
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -13,12 +14,19 @@ class IndexView(generic.ListView):
   context_object_name = 'latest_question_list'
 
   def get_queryset(self):
-    return Question.objects.order_by('-pub_date')
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
   
 
 class DetailView(generic.DetailView):
   model = Question
   template_name = 'polls/detail.html'
+
+  def get_queryset(self):
+    """
+    Excludes any questions that aren't published yet.
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now())
+  
 
 class ResultsView(generic.DetailView):
   model = Question
